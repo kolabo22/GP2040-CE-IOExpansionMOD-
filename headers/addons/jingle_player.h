ec50279ea21d524ee401faa585bfba39af28d082
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-21 00:06:21 +0900

    Update jingle_player.h

--- headers/addons/jingle_player.h
+++ headers/addons/jingle_player.h
@@ -1,27 +1,24 @@
 #ifndef _JINGLE_PLAYER_H_
 #define _JINGLE_PLAYER_H_
 
-#include "gp2040.h"
 #include "gpaddon.h"
-
-#define JINGLE_PLAYER_VPP_PIN 21
-#define JINGLE_PLAYER_BUSY_PIN 20
+#include "GamepadEnums.h"
 
 class JinglePlayerAddon : public GPAddon {
 public:
     virtual bool available() override;
     virtual void setup() override;
     virtual void process() override;
- 	  virtual std::string name() override { return "JinglePlayer"; }
+    virtual void preprocess() override;
+    virtual void postprocess(bool reportSent) override;
+    virtual std::string name() override { return "JinglePlayer"; }
 
 private:
     void sendOneLineCommand(uint8_t cmd);
     void playJingleByMode();
-    void setVolume(uint8_t volume); // 0-30で指定
-    
+    void setVolume(uint8_t volume);
     bool isPlaying();
     uint8_t lastInputMode;
-    uint8_t currentVolume; // Webコンフィグから渡される想定
 };
 
 #endif
