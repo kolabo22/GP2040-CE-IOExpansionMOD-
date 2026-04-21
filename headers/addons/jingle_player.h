0fc108b7cfe0a13e2b849ad394518b996e83fc9f
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-21 23:26:58 +0900

    Update jingle_player.h

--- headers/addons/jingle_player.h
+++ headers/addons/jingle_player.h
@@ -1,26 +1,23 @@
-#ifndef _JINGLE_PLAYER_H_
-#define _JINGLE_PLAYER_H_
+#ifndef JINGLE_PLAYER_H
+#define JINGLE_PLAYER_H
 
-#include "gpaddon.h"
-#include "GamepadEnums.h"
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
 
-class JinglePlayerAddon : public GPAddon {
+#define JQ8900_UART      uart1
+#define JQ8900_TX_PIN    20
+#define JQ8900_RX_PIN    21
+#define JQ8900_BAUD      9600
+
+class JinglePlayer {
 public:
-    virtual bool available() override;
-    virtual void setup() override;
-    virtual void process() override;
-    virtual void preprocess() override;
-		virtual void reinit() override;
-    virtual void postprocess(bool reportSent) override;
-    virtual std::string name() override { return "JinglePlayer"; }
-
+    void setup();
+    void setVolume(uint8_t volume); // 0-30
+    void play(uint16_t trackId);    // 1-65535
+    void stop();
+    
 private:
-    void sendOneLineCommand(uint8_t cmd);
-    void playJingleByMode();
-    void setVolume(uint8_t volume);
-    bool isPlaying();
-    uint8_t lastInputMode;
-    bool bootPlayed;
+    void sendCommand(uint8_t type, uint8_t* data, uint8_t len);
 };
 
 #endif
