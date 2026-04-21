b905db2f1c5aac7ab86f73ae08fd381902227731
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-22 00:08:18 +0900

    Update jingle_player.h

--- headers/addons/jingle_player.h
+++ headers/addons/jingle_player.h
@@ -1,7 +1,7 @@
 #ifndef JINGLE_PLAYER_H
 #define JINGLE_PLAYER_H
 
-#include "gpaddon.h" // GPAddonを継承するために必要
+#include "gpaddon.h"
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
@@ -10,22 +10,19 @@
 #define JQ8900_RX_PIN    21
 #define JQ8900_BAUD      9600
 
-// GPAddon を継承するように変更
-class JinglePlayer : public GPAddon {
+class JinglePlayerAddon : public GPAddon {
 public:
     void setup();
-    void preprocess(); // 毎フレームの処理（Addonシステムに必要）
-    void setVolume(uint8_t volume); // 0-30
-    void play(uint16_t trackId);    // 1-65535
+    void preprocess();
+    void setVolume(uint8_t volume); 
+    void play(uint16_t trackId);    
     void stop();
     
-    // Addonシステム用の識別名
     virtual std::string name() { return "JinglePlayer"; }
 
 private:
     void sendCommand(uint8_t type, uint8_t* data, uint8_t len);
     
-    // 設定値を保持する変数
     bool enabled;
     uint8_t volume;
 };
