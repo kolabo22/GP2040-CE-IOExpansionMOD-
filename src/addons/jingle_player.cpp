0bdc8873268ff4f0a0ab990a6d355b979d0a71e0
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-27 23:14:05 +0900

    Update jingle_player.cpp

--- src/addons/jingle_player.cpp
+++ src/addons/jingle_player.cpp
@@ -2,7 +2,7 @@
 #include "storagemanager.h"
 #include "drivermanager.h"
 
-// スタック破壊を防ぐため、静的領域にバッファを確保
+// 静的バッファでメモリを安定化
 static uint8_t g_uart_buf[10] = {0x7E, 0xFF, 0x06, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xEF};
 
 void JinglePlayerAddon::setup() {
@@ -10,16 +10,16 @@ void JinglePlayerAddon::setup() {
     _hasPlayedOnBoot = false;
     _wasConfigMode = false;
 
-    uart_init(uart0, 9600);
-    gpio_set_function(0, GPIO_FUNC_UART); // TX: GP0
-    gpio_set_function(1, GPIO_FUNC_UART); // RX: GP1
+    // GP20/21は「UART1」です。ここを間違えるとフリーズします。
+    uart_init(uart1, 9600);
+    gpio_set_function(20, GPIO_FUNC_UART); // TX: GP20
+    gpio_set_function(21, GPIO_FUNC_UART); // RX: GP21
 }
 
 void JinglePlayerAddon::process() {
     static uint32_t bootDelay = 0;
 
     if (!_hasPlayedOnBoot) {
-        // 起動直後のループ待機
         if (bootDelay < 50000) { 
             bootDelay++;
             return;
@@ -29,9 +29,9 @@ void JinglePlayerAddon::process() {
         setVolume(this->volume);
 
         if (isConfig) {
-            play(21); 
+            play(21); // 設定モード
         } else {
-            playSelectedModeJingle();
+            playSelectedModeJingle(); // 通常モード
         }
         
         _hasPlayedOnBoot = true;
@@ -54,6 +54,7 @@ void JinglePlayerAddon::playSelectedModeJingle() {
 
 void JinglePlayerAddon::setVolume(uint8_t volume) {
     g_uart_buf[3] = 0x06;
+    g_uart_buf[5] = 0x00;
     g_uart_buf[6] = volume;
     sendCommand(g_uart_buf);
 }
@@ -67,7 +68,7 @@ void JinglePlayerAddon::play(uint16_t index) {
 
 void JinglePlayerAddon::sendCommand(uint8_t* buf) {
     for (int i = 0; i < 10; i++) {
-        uart_putc_raw(uart0, buf[i]);
+        uart_putc_raw(uart1, buf[i]); // UART1を使用
     }
 }
 
