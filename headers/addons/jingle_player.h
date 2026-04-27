be8915eae3396d6156da8be355443028ec3e1b1d
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-27 23:03:03 +0900

    Update jingle_player.h

--- headers/addons/jingle_player.h
+++ headers/addons/jingle_player.h
@@ -2,32 +2,24 @@
 #define _JINGLE_PLAYER_H_
 
 #include "gpaddon.h"
-#include "storagemanager.h"
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
-// 一時的な構造体定義
-struct JingleOptions {
-    bool enabled;
-    uint8_t volume;
-};
-
 class JinglePlayerAddon : public GPAddon {
 public:
-    // 設定が未定義の間は、ビルドを通すために true を返す
     virtual bool available() { return true; }
     virtual void setup();
     virtual void process();
     virtual void preprocess() {}
-    virtual void postprocess(bool) {} // 追加：必須関数
-    virtual void reinit() {}          // 追加：必須関数
+    virtual void postprocess(bool reportSent);
+    virtual void reinit();
     virtual std::string name() { return "JinglePlayer"; }
 
 private:
     void playSelectedModeJingle();
     void setVolume(uint8_t volume);
     void play(uint16_t index);
-    void sendCommand(uint8_t buf[10]);
+    void sendCommand(uint8_t* buf);
 
     uint8_t volume;
     bool _hasPlayedOnBoot;
