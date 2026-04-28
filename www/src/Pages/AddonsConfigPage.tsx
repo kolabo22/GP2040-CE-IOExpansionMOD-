6a01134faf73d205312e10608a204c960a46b432
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-04-29 00:47:12 +0900

    Update AddonsConfigPage.tsx

--- www/src/Pages/AddonsConfigPage.tsx
+++ www/src/Pages/AddonsConfigPage.tsx
@@ -6,9 +6,9 @@ import * as yup from 'yup';
 import { AppContext } from '../Contexts/AppContext';
 import WebApi from '../Services/WebApi';
 
-// インポートパスとファイル名を標準的な大文字・小文字の組み合わせに修正
+// ファイル名の大文字小文字を GP2040-CE の標準（BoardLED, BOOTSELButton 等）に修正
 import Analog from '../Addons/Analog';
-import BoardLed from '../Addons/BoardLed';
+import BoardLed from '../Addons/BoardLED'; // Led -> LED に修正
 import BOOTSELButton from '../Addons/BOOTSELButton'; // Bootsel -> BOOTSEL に修正
 import BuzzerSpeaker from '../Addons/BuzzerSpeaker';
 import DualInput from '../Addons/DualInput';
@@ -132,7 +132,7 @@ export default function AddonsConfigPage() {
 					<ExtraButtonConfiguration values={values} handleChange={handleChange} />
 					<Tilt values={values} handleChange={handleChange} handleCheckbox={handleCheckbox} />
 					<BoardLed values={values} handleChange={handleChange} />
-					<BOOTSELButton values={values} handleChange={handleChange} /> 
+					<BOOTSELButton values={values} handleChange={handleChange} />
 					<KeyboardHost values={values} handleChange={handleChange} setFieldValue={setFieldValue} />
 					<HETrigger values={values} handleChange={handleChange} handleCheckbox={handleCheckbox} setFieldValue={setFieldValue} />
 					<JinglePlayer values={values} handleChange={handleChange} handleCheckbox={handleCheckbox} setFieldValue={setFieldValue} />
