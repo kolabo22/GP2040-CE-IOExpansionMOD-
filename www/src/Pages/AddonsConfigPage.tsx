dbae023ab5ef08735dea08aff1325beaea207704
kolabo22 <108445657+kolabo22@users.noreply.github.com>
2026-06-25 20:36:39 +0900

    Update AddonsConfigPage.tsx

--- www/src/Pages/AddonsConfigPage.tsx
+++ www/src/Pages/AddonsConfigPage.tsx
@@ -123,10 +123,7 @@ const FormContext = ({ setStoredData }) => {
 
 			// 🔥【ラストピース】setValues ではなく resetForm で初期値の壁を完全に破壊して強制反映！
 			const { resetForm } = useFormikContext();
-			resetForm({ 
-  values: mergedData,
-  initialValues: mergedData
-});
+setValues(mergedData);
 
 			setStoredData(JSON.parse(JSON.stringify(mergedData)));
 		}
